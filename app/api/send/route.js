4363e55e5423b1a6cf24eb01618579c515f57558
joel danjuma <leojjad@gmail.com>
2024-01-17 19:30:04 +0100

    Updated Files

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,6 +1,6 @@
 "use server";
 import { EmailTemplate } from "@/components/email-template";
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
